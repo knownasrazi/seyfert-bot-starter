307c9b197a451b74ad5524e33643ca2085d6f0f9
Razi <rajihraju11@gmail.com>
2026-09-06 14:42:32 +0530

    chore: format template under its own biome rules

--- template/src/index.ts
+++ template/src/index.ts
@@ -2,15 +2,15 @@ import "@dotenvx/dotenvx/config";
 import { Client } from "seyfert";
 
 const client = new Client({
-  intents: ["Guilds", "GuildMembers", "GuildMessages", "MessageContent"],
-  locations: {
-    base: "src",
-    commands: "commands",
-  },
+	intents: ["Guilds", "GuildMembers", "GuildMessages", "MessageContent"],
+	locations: {
+		base: "src",
+		commands: "commands",
+	},
 });
 
 client.once("ready", () => {
-  console.log("[seyfert] bot is online");
+	console.log("[seyfert] bot is online");
 });
 
 void client.start();
